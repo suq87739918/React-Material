d2fba188a8b08489180d16328029d4513e9b1c49
Yueqian Sun <sunyueqian991211@gmail.com>
2023-09-17 19:46:38 -0400

    new code

---
+++ 04 Project eat and split/project-eat-and-split/src/App.js
@@ -0,0 +1,13 @@
+import Split from "./component/Split";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <Split />
+      </div>
+    </div>
+  );
+}
+
+export default App;
