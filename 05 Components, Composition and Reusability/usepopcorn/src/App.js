33446e18729f423d476be4c292d12aae209beb16
Yueqian Sun <sunyueqian991211@gmail.com>
2023-10-03 17:14:35 -0400

    new code

--- 05 Components, Composition and Reusability/usepopcorn/src/App.js
+++ 05 Components, Composition and Reusability/usepopcorn/src/App.js
@@ -320,11 +320,58 @@ function WatchedList({ watched }) {
 }
 
 function MovieDetails({ selectedID, onCloseMovie }) {
+  const [movie, setMovies] = useState({});
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(function () {
+    async function getMovieDetails() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}`
+      );
+      const data = await res.json();
+      setMovies(data);
+    }
+    getMovieDetails();
+  }, []);
+
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
+      <header>
+        <button className="btn-back" onClick={onCloseMovie}>
+          &larr;
+        </button>
+        <img src={poster} alt={`poster of ${movie} movie`}></img>
+        <div className="details-overview">
+          <h2>{title}</h2>
+          <p>
+            {released} &bull: {runtime}
+          </p>
+          <p>{genre}</p>
+          <p>
+            <span>⭐️</span>
+            {imdbRating} IMDb rating
+          </p>
+        </div>
+      </header>
+      <section>
+        <p>
+          <em>{plot}</em>
+        </p>
+        <p>Starring {actors}</p>
+        <p>Directed by {director}</p>
+      </section>
       {selectedID}
     </div>
   );
