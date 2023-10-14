9127b9f5a919f72390bc8bbafc5abad033e0a4c9
Yueqian Sun <sunyueqian991211@gmail.com>
2023-10-13 20:14:34 -0400

    new code

---
+++ 08 useReducer Hook/react-quiz/src/components/FinishScreen.js
@@ -0,0 +1,30 @@
+function FinishScreen({ points, maxPossiblePoints, highscore }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  let emoji;
+  if (percentage === 100) {
+    emoji = "👍";
+  }
+  if (percentage >= 80 && percentage < 100) {
+    emoji = "👋";
+  }
+  if (percentage >= 50 && percentage < 80) {
+    emoji = "🥳";
+  }
+  if (percentage >= 0 && percentage < 50) {
+    emoji = "😒";
+  }
+  if (percentage === 0) {
+    emoji = "😳";
+  }
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>Your Score <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints}({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore : {highscore} points)</p>
+    </>
+  );
+}
+
+export default FinishScreen;
